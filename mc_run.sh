1b6055eade9bca37886183076a9a15815acd6e46
Brandon <brandon.furtwangler@hotmail.com>
2025-11-12 14:14:54 -0800

    fix

--- mc_run.sh
+++ mc_run.sh
@@ -8,4 +8,8 @@ if [ ! -f ".mc_env" ]; then
     bash mc_setup
 fi
 
-bash run10.sh --mc --mc_train_report 1 --mc_log_timers 1 --mc_log_lod_ascii_val --mc_log_lod_ascii_train 
+bash run10.sh --mc \
+    --mc_train_report 1 \
+    --mc_log_timers 1 \
+    --mc_log_lod_ascii_val 1 \
+    --mc_log_lod_ascii_train 1
